a343a5726e511df2c1a1c9aa17f869176de56f6d
marloqwerty <88598325+marloqwerty@users.noreply.github.com>
2021-08-24 16:47:35 +0800

    Add SCHEDULE
    update the other data

--- meme.js
+++ meme.js
@@ -7,21 +7,35 @@ const meme = [
 		day: 'monday',
 		text: 'It’s Monday Again. >>_<<',
 		schedule: [
-			{ EnviScie: '8:00-9:00', prof: 'Mr. Quack' },
-			{ Ethic: '9:00-10:00', prof: 'Mr. Quack' },
+			{ subject: 'EnviScie', time: '8:00-9:00', prof: 'Mr. Quack' },
+			{ subject: 'Discrete Math', time: '9:00-10:00', prof: 'Mr. Quack' },
 		],
 	},
 	{
 		day: 'tuesday',
 		text: 'Atleast it’s not Monday',
+		schedule: [
+			{ subject: 'Database System', time: '11:00-12:00', prof: 'Koro-Sensei' },
+			{ subject: 'Data Structure', time: '13:00-14:00', prof: 'Kakashi' },
+		],
 	},
 	{
 		day: 'wednesday',
 		text: 'Atleast it’s not Monday',
+		schedule: [
+			{ subject: 'Envi Sci', time: '8:00-9:00', prof: 'Kakashi' },
+			{ subject: 'Discrete Math', time: '9:00-10:00', prof: 'Master Roshi' },
+			{ subject: 'Database System', time: '14:00-17:00', prof: 'Koro-Sensei' },
+		],
 	},
 	{
 		day: 'thursday',
 		text: '...',
+		schedule: [
+			{ subject: 'Data Structure', time: '8:00-9:00', prof: 'Kakashi' },
+			{ subject: 'Discrete Math', time: '9:00-10:00', prof: 'Master Roshi' },
+			{ subject: 'Database System', time: '14:00-17:00', prof: 'Koro-Sensei' },
+		],
 	},
 	{
 		day: 'friday',
@@ -34,5 +48,3 @@ const meme = [
 ];
 
 export default meme;
-
-
