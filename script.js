6fa313033107cf2f24fd356eade212557df5462e
marloqwerty <88598325+marloqwerty@users.noreply.github.com>
2021-08-27 17:29:18 +0800

    js updated as of 08-27-21
    I wish it will work

--- script.js
+++ script.js
@@ -6,46 +6,73 @@ const container = document.querySelector('.text');
 const day = new Date();
 
 let startSlider = (position) => {
-	var col = [];
-	for (var i = 0; i < meme[position].schedule.length; i++) {
-		for (var key in meme[position].schedule[i]) {
+	let col = [];
+
+	meme[position].schedule.forEach((item) => {
+		for (let key in item) {
 			if (col.indexOf(key) === -1) {
 				col.push(key);
 			}
 		}
-	}
+	});
 
-	// Create a table.
-	var table = document.createElement('table');
+	// for (let i = 0; i < meme[position].schedule.length; i++) {
+	// 	meme[position].schedule[i].forEach((key) => {
+	// 		if (col.indexOf(key) === -1) {
+	// 			col.push(key);
+	// 		}
+	// 	});
+	// }
 
-	// Create table header row using the extracted headers above.
-	var tr = table.insertRow(-1); // table row.
+	const table = document.createElement('table');
 
-	for (var i = 0; i < col.length; i++) {
-		var th = document.createElement('th'); // table header.
+	let tr = table.insertRow(-1); // table row.
+
+	for (let i = 0; i < col.length; i++) {
+		let th = document.createElement('th');
 		th.innerHTML = col[i];
+
 		tr.appendChild(th);
 	}
 
-	// add json data to the table as rows.
-	for (var i = 0; i < meme[position].schedule.length; i++) {
-		tr = table.insertRow(-1);
+	// json data to the table as rows.
 
-		for (var j = 0; j < col.length; j++) {
-			var tabCell = tr.insertCell(-1);
-			tabCell.innerHTML = meme[position].schedule[i][col[j]];
+	meme[position].schedule.forEach((key) => {
+		tr = table.insertRow(-1);
+		tr.classList.add(key.subject.replace(/\s+/g, '_'));
+		// words.replace(/\s+/g, '') replaces whitespace with non-whitespace or remove whitespace
+		for (let j = 0; j < col.length; j++) {
+			let tabCell = tr.insertCell(-1);
+			tabCell.innerHTML = key[col[j]];
 		}
-	}
+	});
 
 	// Now, add the newly created table with json data, to a container.
-	var divShowData = document.getElementById('showData');
+	let divShowData = document.getElementById('showData');
 	divShowData.innerHTML = '';
 	divShowData.appendChild(table);
+	container.innerHTML = `
+	
+	<h1>${meme[position].day}</h1>
+	<h2>${meme[position].text}</h2>
+
+	`;
 };
 
-startSlider(3);
-// startSlider(day.getDay());
-
+// startSlider(2);
+startSlider(day.getDay());
+let daysOfWeek = [];
+let selectSubject = document.getElementById('subject-select');
+meme.forEach((item) => {
+	selectSubject.add(new Option(item.day.toUpperCase(), item.day));
+	daysOfWeek.push(item.day);
+});
+selectSubject.onchange = () => {
+	console.log(selectSubject.value);
+	startSlider(daysOfWeek.indexOf(selectSubject.value));
+};
+console.log(daysOfWeek[day.getDay()]);
+selectSubject.value = daysOfWeek[day.getDay()];
 /*   var p = {
 "p1": "value1",
 "p2": "value2",
